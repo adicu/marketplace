79a306fb56eb796b743056e619be19758e548886
Maclyn Brandwein <me@maclyn.me>
2015-02-13 16:36:28 -0500

    Initial file setup. Initial Vagrantfile and Python setup defined. A lot based on Density's files.

---
+++ config/bootstrap.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Installs the package passed in if it's not installed
+install () {
+	package=$1
+	dpkg-query -l $package &> /dev/null
+	if [ $? -ne 0 ]; then
+	apt-get -y install $package
+	fi
+}
+
+apt-get update
+
+# Install git
+install git-core
+install git
+
+# Install Python
+apt-get install -y python \
+python-pip \
+python-dev \
+python-software-properties \
+libpq-dev
+apt-get update
+
+# Install SQLite
+apt-get install -y sqlite3
+apt-get install -y libsqlite3-dev
+
+# Install Python libraries
+pip install -r /vagrant/config/requirements.txt
+pip install flake8 # For local testing
+pip install SQLAlchemy # Install SQLAlchemy
+
+# Install Vim
+install vim
---
+++ marketplace/marketplace.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, render_template, json, request
+
+
+app = Flask(__name__)
+
+import sqlalchemy
+
+# Import configuration from Flask
+app.config.update(
+	DEBUG=True,
+	HOST='0.0.0.0'
+)
+
+@app.route('/')
+def index():
+	return "Marketplace"
+
+if __name__ == '__main__':
+	app.run(host=app.config['HOST'])

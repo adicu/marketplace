7b46c6d351ccf2b9899cd12d8025c452c4813a44
Maclyn Brandwein <me@maclyn.me>
2015-04-17 17:09:57 -0400

    Set up per-route auth where it'd make sense and add some methods to mark items as sold. Clean up item rating.

--- marketplace/routes/auth.py
+++ marketplace/routes/auth.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify, render_template, json, request, redirect
+from flask import Blueprint, jsonify, render_template, json, request, redirect, session, url_for
 from marketplace import db, flow
 from marketplace.models import User
+from marketplace.routes import home
 from oauth2client.client import OAuth2WebServerFlow
 from datetime import date, datetime
 from flask.ext.restful import reqparse  # Parse through requests
@@ -11,6 +12,21 @@ auth = Blueprint('auth', __name__)
 
 # From Density project
 CU_EMAIL_REGEX = r"^(?P<uni>[a-z\d]+)@.*(columbia|barnard)\.edu$"
+
+
+@auth.route("/logout")
+def logout():
+    session.pop("username", None)
+    session["logged_in"] = False
+    return "logged out" #home.home_page()
+
+
+@auth.route("/session_test")
+def session_test():
+    if "username" in session:
+        return session["username"]
+    else:
+        return "You're not logged in."
 
 
 @auth.route("/login/google")
@@ -28,28 +44,10 @@ def google_login():
         email = data["emails"][0]["value"]
         return signin(email)
     elif error:  # Google login returned an error; alert the user
-        return error
+        return render_template("index.html", logged_in=False, 
+                                error_message="An error occcurred authenticating you.")
     else:  # We just got to the code in the first place
         return redirect(flow.step1_get_authorize_url())
-
-
-@auth.route('/rate_user/<user_id>/<rating>')
-def rate_user(user_id, rating):
-    # Get matching users
-    rating = int(rating)
-    matching_users = User.query.filter_by(user_id=user_id).all()
-    if len(matching_users) == 0:
-        return "No matching users found!"
-    matching_users[0].num_ratings += 1
-    if rating > 0:
-        matching_users[0].rating_value += 1
-    elif rating < 0:
-        matching_users[0].rating_value -= 1
-    db.session.commit()
-
-    # Percent formula is here (average is 0.5)
-    return "New rating achieved; percent is: " \
-           + str(float(matching_users[0].rating_value) / float(2 * matching_users[0].num_ratings) + 0.5)
 
 
 def signin(email):
@@ -61,6 +59,8 @@ def signin(email):
     if matches == 0:
         return register(email)
     elif matches == 1:
+        session["username"] = matching_users[0].user_id
+        session["logged_in"] = True
         return jsonify(data=[user.serialize for user in matching_users])
     return "Error!"
 
@@ -75,9 +75,12 @@ def register(email):
         if not regex_result:
             raise NameError("Invalid e-mail")
 
-        user = User(email, 50.0, 0)
+        user = User(email, 0, 0)
         db.session.add(user)
         db.session.commit()
+
+        session["username"] = email
+        session["logged_in"] = True
         return "Welcome to Marketplace!"
     except Exception:
         return "We couldn't register you. Make sure you use a Columbia or Barnard email."
--- marketplace/routes/insert.py
+++ marketplace/routes/insert.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from marketplace import db
+from marketplace import db, session
 from marketplace.models import Item
 from datetime import date, datetime
 
@@ -8,10 +8,27 @@ insert = Blueprint('insert', __name__) # Leave of URL prefix intentionally
 
 @insert.route('/insert/<user_id>/<name>/<description>/<image_url>/<float:price>')
 def insert_item(user_id, name, description, image_url, price):
-    new_item = Item(user_id, name, description, price, image_url, date.today(), date.today(), 'listed')
+    new_item = Item(user_id, name, description, image_url, price, date.today(), date.today(), None, 'listed')
     db.session.add(new_item)
     db.session.commit()
     return "Item added with id: " + str(new_item.id)
+
+
+@insert.route('/mark_sold/<item_id>/<sold_to>')
+def mark_sold(item_id, sold_to):
+    item = Item.query.filter_by(id=item_id).all()
+    if len(item) == 0:
+        return "No item found!"
+    if item[0].user_id != session["username"]:
+        return "You can't mark someone else's item as sold. Bad."
+    if item[0].status != "listed":
+        return "You can't mark this item as sold. It's in state " + item[0].status + "."
+    if sold_to == session["username"]:
+        return "You can't sell an item to yourself."
+    item[0].status = "sold"
+    item[0].sold_to = sold_to
+    db.session.commit()
+    return "Marked as sold."
 
 
 @insert.route('/bulk_test')
@@ -19,7 +36,7 @@ def bulk_test():
     for i in range(100):
         new_item = Item("uni123", "Product name", "Product description",
                         "http://www.dunbartutoring.com/wp-content/themes/thesis/rotator/sample-1.jpg",
-                        i, datetime.utcnow(), datetime.utcnow(),
+                        i, datetime.utcnow(), datetime.utcnow(), None,
                         'listed')
         db.session.add(new_item)
     db.session.commit()
